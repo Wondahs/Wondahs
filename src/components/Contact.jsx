0a78261f5c5ff065c2489ff445edc522b0f2625b
Wonders Victor <wondersprince1@gmail.com>
2024-10-22 16:42:33 +0100

    Testing contact form with formspree

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,19 @@
 import "../assets/styles/Contact.css";
-import Socials from './Socials';
+import Socials from "./Socials";
 import locationIcon from "../assets/images/icons/location-svgrepo-com.svg";
 import emailIcon from "../assets/images/icons/email-icon.svg";
 import phoneIcon from "../assets/images/icons/phone-icon.svg";
+import { useForm, ValidationError } from "@formspree/react";
+import { useRef } from "react";
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("meoqokzb");
+  const formRef = useRef(null);
+
+  if (state.succeeded) {
+    formRef.current.reset();
+    alert("Message sent successfully! I'll get back to you soon.");
+  }
   return (
     <section className="contact" id="contact">
       <h5 className="highlight">CONTACT</h5>
@@ -14,12 +23,38 @@ const Contact = () => {
         hello, feel free to reach out. Fill out the form below, and I&apos;ll
         get back to you as soon as possible!
       </p>
-      <form className="contact-form">
-        <input type="text" placeholder="Name" required />
-        <input type="email" placeholder="Email" required />
-        <input type="text" placeholder="Subject" required />
-        <textarea name="Message" id="" placeholder="Message"></textarea>
-        <button type="button">SUBMIT</button>
+      <form className="contact-form" ref={formRef} onSubmit={handleSubmit}>
+        <input id="name" name="name" type="text" placeholder="Name" required />
+        <ValidationError prefix="Name" field="name" errors={state.errors} />
+        <input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          placeholder="Subject"
+          required
+        />
+        <ValidationError
+          prefix="Subject"
+          field="subject"
+          errors={state.errors}
+        />
+        <textarea name="Message" id="message" placeholder="Message"></textarea>
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button type="submit" disabled={state.submitting}>
+          SUBMIT
+        </button>
       </form>
       <div className="contacts">
         <div className="contact-card">
